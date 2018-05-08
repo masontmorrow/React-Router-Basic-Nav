683096799da269c8329cf1cc29b19def1427522d
masontmorrow <masontmorrow@gmail.com>
2018-05-08 21:52:28 +0200

    Completed code-along

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 import Navigation from './Navigation';
+import { BroswerRouter as Router} from 'react-router-dom';
 
 export { Home, About, Contact, Navigation };
